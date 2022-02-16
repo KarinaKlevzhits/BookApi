2cd815f89a8063c6c7b7538ab4326475c9c6bd5c
Karina <karinaklevzhits@gmail.com>
2022-02-16 11:41:03 +0300

    BS-01/add connection

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
 class Book < ApplicationRecord
   belongs_to :genre
+  belongs_to :author
 end
--- db/migrate/20220216083232_create_books.rb
+++ db/migrate/20220216083232_create_books.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
       t.string :name
       t.integer :year
       t.integer :quantity
-      t.refernces :author
-      t.references :genre, null: false, foreign_key: true
+      t.references :author
+      t.references :genre
 
       t.timestamps
     end
